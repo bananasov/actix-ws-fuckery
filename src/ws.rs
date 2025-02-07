5a7ab41ae48a0287d1fb46f7d164341086bf4f91
sovnya <enp7s1@tutanota.com>
2025-02-07 21:46:44 +0100

    Woops, update token expiration time to 30 seconds

--- src/ws.rs
+++ src/ws.rs
@@ -19,7 +19,7 @@ use crate::models::websocket::{
 
 const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
 const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
-const TOKEN_EXPIRATION: Duration = Duration::from_secs(10);
+const TOKEN_EXPIRATION: Duration = Duration::from_secs(30);
 
 #[derive(Clone)]
 pub struct WebSocketServer {
